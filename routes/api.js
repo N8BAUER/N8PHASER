0ecac35fb2f1c694da276020b44599ae67f4e3a5
Nate <n8bdoesemailvia@gmail.com>
2018-01-29 23:45:33 -0700

    oopise api key since I forgot to list the env in gitignore

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,47 @@
-var express = require('express')
-var router = express.Router()
+var express = require("express");
+var router = express.Router();
+var helper = require("sendgrid").mail;
 
-router.get('/:action', function(req, res, next) {
+router.get("/:action", function(req, res, next) {
+  var action = req.params.action;
 
-  var action = req.params.action
+  if (action == "send") {
+    //send an email
+    var from_email = new helper.Email("n8bdoesemailvia@gmail.com");
+    var to_email = new helper.Email("n8bdoesemailvia@gmail.com");
+    var subject = "This is the start of the email server! Ty SG";
+    var content = new helper.Content("text/html", "Hello, from N8BAUER.COM!");
+    var mail = new helper.Mail(from_email, subject, to_email, content);
 
-  if (action == 'send'){ //send an email
-    res.json({
-      confirmation: 'Success',
-      action: action
-    })
+    var sg = require("sendgrid")(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+      method: "POST",
+      path: "/v3/mail/send",
+      body: mail.toJSON()
+    });
 
-    return
+    sg.API(request, function(error, response) {
+      if (error){
+        res.json({
+          confirmation: 'fail',
+          message: error
+        })
+        return
+      }
+
+      res.json({
+        confirmation: 'Success',
+        response: response
+      })
+      return
+    });
+
+    return;
   }
   res.json({
-    confirmation: 'Success',
-    message: 'Invalid Action'
-  })
-})
+    confirmation: "Success",
+    message: "Invalid Action"
+  });
+});
 
 module.exports = router;
