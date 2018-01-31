b8a55c27c81428261c8d8d06d7d78fd940576a3d
Nate <n8bdoesemailvia@gmail.com>
2018-01-31 02:28:34 -0700

    testing theory of serialize()

--- Public/portfolio.js
+++ Public/portfolio.js
@@ -1,16 +1,16 @@
-  const emailAPI= "https://n8-email.herokuapp.com"
-
-$(document).ready(function() {
-  console.log("jQuery working!")
-
-  $(".display-6").hide()
-
-  $('body').on('submit', '#emailForm', function(e){
-  e.preventDefault();
-  $.post(emailAPI + "/api/send", $('#emailForm').serialize())
-  .then(function(response) {
-  $(".btn").fadeOut(1000)
-  $(".display-6").fadeIn(2500)
-  })
-})
-});
+//   const emailAPI= "https://n8-email.herokuapp.com"
+//
+// $(document).ready(function() {
+//   console.log("jQuery working!")
+//
+//   $(".display-6").hide()
+//
+//   $('body').on('submit', '#emailForm', function(e){
+//   e.preventDefault();
+//   $.post(emailAPI + "/api/send", $('#emailForm').serialize())
+//   .then(function(response) {
+//   $(".btn").fadeOut(1000)
+//   $(".display-6").fadeIn(2500)
+//   })
+// })
+// });
