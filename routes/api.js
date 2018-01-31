b7ba55a623e05edbc4aac6f1b097470f0f3d8718
Nate <n8bdoesemailvia@gmail.com>
2018-01-31 15:35:21 -0700

    cors

--- routes/api.js
+++ routes/api.js
@@ -1,32 +1,48 @@
-var express = require("express");
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 var utilities = require('../utilities')
 
-router.post("/:action", function(req, res, next) {
-  var action = req.params.action;
 
-  if (action == "send") {
-    //send an email
-    utilities.email
-    .sendemail(req.body)
-    .then(function(response){
-        res.json({
-          confirmation: 'Success',
-          response: response
-        })
-    })
-    .catch(function(err){
-      res.json({
-        confirmation: 'Fail',
-        message: err
-      })
-    })
-    return;
-  }
-  res.json({
-    confirmation: "Success",
-    message: "Invalid Action"
-  });
-});
+router.post('/:action', function(req, res, next) {
 
-module.exports = router;
+	var action = req.params.action
+
+	if (action == 'send'){ // send an email
+
+		utilities.email.sendemail(req.body, function(){
+		    res.json({
+		    	confirmation: 'success',
+		    	message: 'Emails Sent'
+		    })
+		})
+
+
+
+		// utils.Email
+		// .sendEmail(req.body)
+		// .then(function(response){
+		    // res.json({
+		    // 	confirmation: 'success',
+		    // 	response: response
+		    // })
+		// })
+		// .catch(function(err){
+		//     res.json({
+		//     	confirmation: 'fail',
+		//     	message: err
+		//     })
+		// })
+
+		return
+	}
+
+    res.json({
+    	confirmation: 'fail',
+    	message: 'Invalid Action'
+    })
+
+})
+
+
+
+module.exports = router
--- utilities/email.js
+++ utilities/email.js
@@ -3,30 +3,31 @@ var Promise = require('bluebird')
 
 module.exports = {
 
-  sendemail: function(emailInfo){
 
-    return new Promise(function(resolve, reject ){
-      var from_email = new helper.Email(emailInfo.sender);
-      var to_email = new helper.Email("n8bdoesemailvia@gmail.com");
-      var subject = emailInfo.subject;
-      var content = new helper.Content("text/html", emailInfo.message);
-      var mail = new helper.Mail(from_email, subject, to_email, content);
+	sendemail: function(emailInfo){
+		return new Promise(function(resolve, reject){
+			var from_email = new helper.Email(sender.emailInfo)
+			var to_email = new helper.Email("n8bdoesemailvia@gmail.com")
+			var subject = emailInfo.subject
+			var content = new helper.Content('text/html', emailInfo.message)
+			var mail = new helper.Mail(from_email, subject, to_email, content)
 
-      var sg = require("sendgrid")(process.env.SENDGRID_API_KEY);
-      var request = sg.emptyRequest({
-        method: "POST",
-        path: "/v3/mail/send",
-        body: mail.toJSON(),
-      });
+			var sg = require('sendgrid')(process.env.SENDGRID_API_KEY)
+			var request = sg.emptyRequest({
+				method: 'POST',
+				path: '/v3/mail/send',
+				body: mail.toJSON(),
+			})
 
-      sg.API(request, function(error, response) {
-        if (error){
-        reject(error)
-          return
-        }
+			sg.API(request, function(error, response) {
+				if (error){
+					reject(error)
+					return
+				}
 
-        resolve(response)
-      })
-    })
-  }
+				resolve(response)
+			})
+		})
+	}
+
 }
