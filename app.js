ca26468c512818e2fd3cf1a9c3f9fece2d2b758c
Nate <n8bdoesemailvia@gmail.com>
2018-01-30 23:37:44 -0700

    static file change

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 app.use(cors({origin: (domain, callback) => {
   callback(null, true);
 }}));
