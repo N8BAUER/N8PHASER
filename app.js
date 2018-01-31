a15a0e62aff40410234ba66474272e982bf773f0
Nate <n8bdoesemailvia@gmail.com>
2018-01-31 15:08:06 -0700

    app.js config

--- app.js
+++ app.js
@@ -21,12 +21,13 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'Public')));
 
 
 
-app.use("/", routes)
-app.use("/api", api)
+app.use('/', routes)
+app.use('/api', api)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
