4a70ab29bde82fdf2b82dd740a45c03d32ab65e5
Nate <n8bdoesemailvia@gmail.com>
2018-01-31 03:12:05 -0700

    work

--- routes/api.js
+++ routes/api.js
@@ -1,40 +1,27 @@
 var express = require("express");
 var router = express.Router();
-var helper = require("sendgrid").mail;
+var utilities = require('../utilities')
 
 router.post("/:action", function(req, res, next) {
   var action = req.params.action;
 
   if (action == "send") {
     //send an email
-    var from_email = new helper.Email(req.body.sender);
-    var to_email = new helper.Email("n8bdoesemailvia@gmail.com");
-    var subject = req.body.subject;
-    var content = new helper.Content("text/html", req.body.message);
-    var mail = new helper.Mail(from_email, subject, to_email, content);
-
-    var sg = require("sendgrid")(process.env.SENDGRID_API_KEY);
-    var request = sg.emptyRequest({
-      method: "POST",
-      path: "/v3/mail/send",
-      body: mail.toJSON()
-    });
-
-    sg.API(request, function(error, response) {
-      if (error){
+    utilities.email
+    .sendemail(req.body)
+    .then(function(response){
         res.json({
-          confirmation: 'fail',
-          message: error
+          confirmation: 'Success',
+          response: response
         })
-        return
-      }
-
+    })
+    .catch(function(err){
       res.json({
-        confirmation: 'Success',
-        response: response
+        confirmation: 'Fail',
+        message: err
       })
-      return
-    });
+    })
+
 
     return;
   }
@@ -43,6 +30,3 @@ router.post("/:action", function(req, res, next) {
     message: "Invalid Action"
   });
 });
-
-
-module.exports = router;
---
+++ utilities/index.js
@@ -0,0 +1,5 @@
+var email = require('./email')
+
+module.exports = {
+  email: email
+}
