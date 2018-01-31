1da270a98eefd93ec5d3b49b6efff35dbc17ca6d
Nate <n8bdoesemailvia@gmail.com>
2018-01-30 22:50:56 -0700

    removed old code

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var router = express.Router();
 var utility = require('../utility')
-// var helper = require("sendgrid").mail;
 
 router.post("/:action", function(req, res, next) {
   var action = req.params.action;
@@ -22,32 +21,7 @@ router.post("/:action", function(req, res, next) {
         message: err
       })
     })
-    // var from_email = new helper.Email(req.body.email);
-    // var to_email = new helper.Email("n8bdoesemailvia@gmail.com");
-    // var subject = req.body.subject;
-    // var content = new helper.Content("text/html", req.body.message);
-    // var mail = new helper.Mail(from_email, subject, to_email, content);
-    //
-    // var sg = require("sendgrid")(process.env.SENDGRID_API_KEY);
-    // var request = sg.emptyRequest({
-    //   method: "POST",
-    //   path: "/v3/mail/send",
-    //   body: mail.toJSON()
-    // });
-    //
-    // sg.API(request, function(error, response) {
-    //   if (error){
-    //     res.json({
-    //       confirmation: 'fail',
-    //       message: error
-    //     })
-    //     return
-    //   }
-    //
-    //   res.json({
-    //     confirmation: 'Success',
-    //     response: response
-    //   })
+
 
     return;
   }
