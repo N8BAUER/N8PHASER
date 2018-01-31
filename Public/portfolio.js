4d9a43a2aee128821a0624c864c0fccee4b87eae
Nate <n8bdoesemailvia@gmail.com>
2018-01-31 14:47:57 -0700

    samplng html action method

--- Public/portfolio.js
+++ Public/portfolio.js
@@ -1,14 +1,14 @@
-//   const emailAPI= "https://n8-email.herokuapp.com/api/send"
-//
-// $(document).ready(function() {
-//   $(".display-6").hide()
-//
-//   $('body').on('submit', '#emailForm', function(e){
-//   e.preventDefault();
-//   $.post('/api/send', $('#emailForm').serialize())
-//   .then(function(response) {
-//   $(".btn").fadeOut(700)
-//   $(".display-6").fadeIn(2000)
-//   })
-// })
-// });
+  const emailAPI= "https://n8-email.herokuapp.com/api/send"
+
+$(document).ready(function() {
+  $(".display-6").hide()
+
+  $('body').on('submit', '#emailForm', function(e){
+  e.preventDefault();
+  $.post('/api/send', $('#emailForm').serialize())
+  .then(function(response) {
+  $(".btn").fadeOut(700)
+  $(".display-6").fadeIn(2000)
+  })
+})
+});
