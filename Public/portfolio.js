1fd0a30121aeca30426f9019a15a6ddf37510a56
Nate <n8bdoesemailvia@gmail.com>
2018-01-31 02:11:58 -0700

    ...

--- Public/portfolio.js
+++ Public/portfolio.js
@@ -1,14 +1,13 @@
-
+  const emailAPI= "https://n8-email.herokuapp.com"
 
 $(document).ready(function() {
-  const emailAPI= "https://n8-email.herokuapp.com/api/send"
   console.log("jQuery working!")
 
   $(".display-6").hide()
 
   $('body').on('submit', '#emailForm', function(e){
   e.preventDefault();
-  $.post(emailAPI, $('#emailForm').serialize())
+  $.post(emailAPI + "/api/send", $('#emailForm').serialize())
   .then(function(response) {
   $(".btn").fadeOut(1000)
   $(".display-6").fadeIn(2500)
